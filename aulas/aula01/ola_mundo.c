6b3258efa09a5710ffee639dbb63d979c2fd4814
Gabriela-albino90 <gabriela.albino@iesb.edu.br>
2025-02-26 21:41:53 -0300

    codigos da aula01

---
+++ aulas/aula01/ola_mundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // inclusao de bibliotecas
+/*
+Aula01 - Estrutura basica de um programa em C
+*/
+//funcao principal que inicia o programa
+int main() {
+    printf("Ola pessoal!\n");
+    return 0;
+}
