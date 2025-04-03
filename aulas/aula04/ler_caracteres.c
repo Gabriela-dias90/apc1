fe5564cd97e0a6efe330c2fc050dde3ecbb8d97b
Gabriela-albino90 <gabriela.albino@iesb.edu.br>
2025-04-02 21:58:37 -0300

    Adicionando arquivos da pratica04

---
+++ aulas/aula04/ler_caracteres.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    char tecla;
+
+    printf("Pressione uma tecla e depois ENTER: ");
+    scanf("%c", &tecla); // nao sabe tecla = valor entao precisa acessar endereco da variavel com o operador &
+    getchar();
+    printf("Voce pressionou a tecla'%c'\n", tecla);
+
+   
+    char nome[31];
+    printf("Informe seu nome: ");
+    scanf("%[^\n]s", nome);
+    printf("Ola %s!\n", nome);
+
+
+
+
+
+        
+
+
+    return 0;
+}
